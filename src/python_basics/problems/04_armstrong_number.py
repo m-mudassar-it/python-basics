75eee70e65299575c8e2d302dabe2c78d638846c
Muhammad Mudassar <muhammad.mudassar.it@gmail.com>
2025-09-19 19:18:39 +0500

    Create 04_armstrong_number.py

---
+++ src/python_basics/problems/04_armstrong_number.py
@@ -0,0 +1,16 @@
+# Check Armstrong number
+
+num = int(input("Enter a number: "))
+order = len(str(num))  # number of digits
+sum_of_powers = 0
+
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum_of_powers += digit ** order
+    temp //= 10
+
+if num == sum_of_powers:
+    print(num, "is an Armstrong number ✅")
+else:
+    print(num, "is not an Armstrong number ❌")
